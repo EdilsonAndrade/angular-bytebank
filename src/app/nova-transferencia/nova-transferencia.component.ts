aeddeb3395870146af02ccff405af00b1dc28ab9
Edilson <eandrade@classy.org>
2021-08-05 17:58:17 -0300

    criando navegação por codigo e por menu

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Output, EventEmitter } from '@angular/core';
 import { ITransferencia } from '../models/transferencia.model';
@@ -12,17 +13,18 @@ export class NovaTransferenciaComponent {
   valor: number;
   destino: string;
 
-  constructor(private service: TransferenciaService) {
+  constructor(private service: TransferenciaService, private router: Router) {
 
   }
   transferir() {
     const transferencia: ITransferencia = { valor: this.valor, destino: this.destino };
 
     this.service.adicionar(transferencia).subscribe((transferencia: ITransferencia) => {
-      console.log(transferencia, 'transferencia adicionada')
+      console.log(transferencia, 'transferencia adicionada');
+      this.router.navigateByUrl('extrato');
     },
       (erro) => {
-        console.log('ocorreu um erro ', JSON.stringify(erro))
+        console.log('ocorreu um erro ', JSON.stringify(erro));
       });
   }
 
